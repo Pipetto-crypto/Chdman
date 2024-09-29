dbe7a684674f208cb95e6628d0ae6348cf187abe
Pipetto-crypto <Pipetto-crypto@pipetto-crypto.com>
2024-09-29 17:10:38 +0200

    Add a popup menu to button in MainActivity to pick between a compress and a batch compress option

--- app/src/main/java/com/chdman/MainActivity.java
+++ app/src/main/java/com/chdman/MainActivity.java
@@ -6,9 +6,13 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.provider.Settings;
 import android.util.Log;
+import android.view.ContextMenu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
+import androidx.appcompat.widget.PopupMenu;
 import com.chdman.utils.Chdman;
 import com.chdman.utils.FilePicker;
 import com.chdman.utils.FileUtils;
@@ -20,6 +24,8 @@ import java.util.LinkedList;
 public class MainActivity extends AppCompatActivity {
     
     private FilePicker picker;
+    private PopupMenu.OnMenuItemClickListener menuListener;
+    private View.OnClickListener clickListener;
     private Chdman chdman;
 
     public MainActivity getInstance() {
@@ -49,24 +55,40 @@ public class MainActivity extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         picker = new FilePicker(getInstance());
+        MenuItem.OnMenuItemClickListener listener;
         chdman = new Chdman(getInstance());
         setContentView(R.layout.activity_main);
         enableStoragePermission();
         extractChdmanBinary();
-        FloatingActionButton btn = findViewById(R.id.fab);
-        btn.setOnClickListener(new View.OnClickListener(){
+        menuListener = new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                if (item.getItemId() == R.id.compress) 
+                    picker.pickMultiple("*/*");
+                else if (item.getItemId() == R.id.batch_compress)
+                    picker.pickFolder();
+                return true;
+            }     
+        };
+        clickListener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                picker.pickMultiple("*/*");
-            }          
-        });
+                PopupMenu popup = new PopupMenu(getInstance(), v);
+                popup.setOnMenuItemClickListener(menuListener);  
+                popup.getMenuInflater().inflate(R.menu.popup_menu, popup.getMenu());
+                popup.show();  
+            }    
+        };
+        FloatingActionButton btn = findViewById(R.id.fab);
+        btn.setOnClickListener(clickListener);
     }
+    
     
     @Override
     public void onResume() {
         LinkedList<String> filesList = picker.getFilesList();
         if (filesList.size() > 0) 
-            chdman.batchCompress(filesList);
+            chdman.compress(filesList);
         super.onResume();
     }
 }
